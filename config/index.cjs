6a76c942272e1619da290b527998f95525070133
1Madzzz <129497365+1Madzzz@users.noreply.github.com>
2023-08-27 20:06:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,13 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+// 请将以下代码按照图示粘贴到对应位置
+USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxf9f1855599e626e2',
+  //APP_ID: 'wxf9f1855599e626e2',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'e26c69dad7963729fe40c5b208b8a4c1',
+  //APP_SECRET: 'e26c69dad7963729fe40c5b208b8a4c1',
 
   PROVINCE: '湖北',
   CITY: '武汉',
@@ -21,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '呼呼',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oIEKc6Ph8i6jlY6gSE9PyOlTyVms',
+      id: 'PDU25081TpdtlOpvQLqZwafhafTRHLBaRzpi6GkOs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Lp3r7UXF26fhu6iJvaaRbLdCfqpsA-nRe_z3mG2mnt8',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
